fb975ce5a06eaa708981191e6ac87d62f6e2e35f
Banja David Lotigo <banjadavidlotigo@Banjas-MacBook-Pro.local>
2022-06-08 20:03:55 +0300

    update: edited the app/server file

--- app/server.js
+++ app/server.js
@@ -1,13 +1,13 @@
 // app/server.js
-
-var express = require("express");
-var app = express()
-
-var converter = require("./convertor");
+const express = require("express");
+const app = express()
+const port = 3000; // default
+const converter = require("./convertor");
+const logger = require('../logger/log')
 
 app.get("/rgbToHex", function(req, res) {
-  var red   = parseInt(req.query.red, 10);
-  var green = parseInt(req.query.green, 10);
+  let red   = parseInt(req.query.red, 10);
+  let green = parseInt(req.query.green, 10);
   var blue  = parseInt(req.query.blue, 10);
   var hex = converter.rgbToHex(red, green, blue);
   res.send(hex);
@@ -20,5 +20,14 @@ app.get("/hexToRgb", function(req, res) {
   res.send(JSON.stringify(rgb));
 
 });
+// logger levels
+logger.error("error");
+logger.warn("warn");
+logger.info("info");
+logger.debug("debug");
+logger.verbose("verbose");
+logger.silly("silly");
 
-app.listen(3000);
+app.listen(port, () => {
+  logger.info("server listening on PORT " + port);
+});
